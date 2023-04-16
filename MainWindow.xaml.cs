f22b403e0620764dfea13eadc118c6690379b523
ar278 <ar278103@mail.ru>
2023-04-16 20:49:25 +0300

    Переделал цикл for , который не верно считывал страницы!

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -30,11 +32,10 @@ namespace Parsing
 
         private async void Button_Click_1(object sender, RoutedEventArgs e)
         {
+           
             VacancyListBox.Items.Clear();
             using var client = new HttpClient();
             string htmlBody = await client.GetStringAsync("https://proglib.io/vacancies/all?workType=all&workPlace=all&experience=&salaryFrom=&page=");
-
-
 
             string pattern = "title=\"(.+)20(.+)\"";
             string pattern1 = "\"title\">(.+)<\\/h2>";
@@ -48,10 +49,10 @@ namespace Parsing
             Regex regex = new Regex(pattern);
             Regex regex1 = new Regex(pattern1);
             Regex regex2 = new Regex(pattern2);
-
-            for (int i = 1; i < count + 1; i++)
+            
+            for (int i = 0; i < count; i++)
             {
-                List<string> items = new List<string>();
+                List<string> items = new();
 
                 MatchCollection matches1 = regex1.Matches(htmlBody + i.ToString());
                 foreach (Match match1 in matches1)
@@ -67,13 +68,14 @@ namespace Parsing
                 MatchCollection matches2 = regex2.Matches(htmlBody + i.ToString());
                 for (int j = 0; j < items.Count; j++)
                 {
-                    items[j] += "https://proglib.io/vacancies/" + matches2[j].Groups[1].Value+"\n";
+                    items[j] += "https://proglib.io/vacancies/" + matches2[j].Groups[1].Value + "\n";
                 }
-
+                
                 for (int j = 0; j < items.Count; j++)
                 {
                     VacancyListBox.Items.Add(items[j]);
                 }
+                htmlBody = await client.GetStringAsync($"https://proglib.io/vacancies/all?workType=all&workPlace=all&experience=&salaryFrom=&page={i}");
             }
 
 

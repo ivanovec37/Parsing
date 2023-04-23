d5d0ab342be869377e42842ae8f2a7629404e353
ar278 <ar278103@mail.ru>
2023-04-23 10:54:49 +0300

    Добавил распаралеливание.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,58 +25,72 @@ namespace Parsing
     /// </summary>
     public partial class MainWindow : Window
     {
+       
+       
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private async void GetParallelData(int i)
+        {
+            
+            Regex regex = new Regex("title=\"(.+)20(.+)\"");
+            Regex regex1 = new Regex("\"title\">(.+)<\\/h2>");
+            Regex regex2 = new Regex("<a href=\"/vacancies/(.+)\" class=\"no-link\">");
+            using var client = new HttpClient();
+            List<string> items = new();
+            string htmlBody2 = await client.GetStringAsync($"https://proglib.io/vacancies/all?workType=all&workPlace=all&experience=&salaryFrom=&page={i}");
+
+            MatchCollection matches1 = regex1.Matches(htmlBody2);
+            foreach (Match match1 in matches1)
+            {
+                items.Add(match1.Groups[1].Value + "\n");
+            }
+            MatchCollection matches = regex.Matches(htmlBody2);
+            for (int j = 0; j < items.Count; j++)
+            {
+                items[j] += matches[j].Groups[1].Value + "\n";
+            }
+
+            MatchCollection matches2 = regex2.Matches(htmlBody2);
+            for (int j = 0; j < items.Count; j++)
+            {
+                items[j] += "https://proglib.io/vacancies/" + matches2[j].Groups[1].Value + "\n";
+            }
+
+            for (int j = 0; j < items.Count; j++)
+            {
+                Dispatcher.Invoke(() => VacancyListBox.Items.Add(items[j]));
+            }
+        }
+
         private async void Button_Click_1(object sender, RoutedEventArgs e)
         {
-           
-            VacancyListBox.Items.Clear();
-            using var client = new HttpClient();
-            string htmlBody = await client.GetStringAsync("https://proglib.io/vacancies/all?workType=all&workPlace=all&experience=&salaryFrom=&page=");
-
-            string pattern = "title=\"(.+)20(.+)\"";
-            string pattern1 = "\"title\">(.+)<\\/h2>";
-            string pattern2 = "<a href=\"/vacancies/(.+)\" class=\"no-link\">";
-            string pattern3 = "data-total=\"(.+)\"";
-
-            Regex regex3 = new Regex(pattern3);
-            MatchCollection matches3 = regex3.Matches(htmlBody + "1");
-
-            int count = int.Parse(matches3[0].Groups[1].Value);
-            Regex regex = new Regex(pattern);
-            Regex regex1 = new Regex(pattern1);
-            Regex regex2 = new Regex(pattern2);
-            
-            for (int i = 0; i < count; i++)
+            try
             {
-                List<string> items = new();
-                htmlBody = await client.GetStringAsync($"https://proglib.io/vacancies/all?workType=all&workPlace=all&experience=&salaryFrom=&page={i}");
+                VacancyListBox.Items.Clear();
+                using var client = new HttpClient();
+                string htmlBody = await client.GetStringAsync("https://proglib.io/vacancies/all?workType=all&workPlace=all&experience=&salaryFrom=&page=");
+                Regex regex3 = new Regex("data-total=\"(.+)\"");
+                MatchCollection matches3 = regex3.Matches(htmlBody + "1");
 
-                MatchCollection matches1 = regex1.Matches(htmlBody);
-                foreach (Match match1 in matches1)
+                int count = int.Parse(matches3[0].Groups[1].Value);
+
+                List<int>pageNumbers = new List<int>();
+                for(int i = 1; i < count+1;i++)
                 {
-                    items.Add(match1.Groups[1].Value + "\n");
-                }
-                MatchCollection matches = regex.Matches(htmlBody);
-                for (int j = 0; j < items.Count; j++)
-                {
-                    items[j] += matches[j].Groups[1].Value + "\n";
+                    pageNumbers.Add(i);
                 }
 
-                MatchCollection matches2 = regex2.Matches(htmlBody);
-                for (int j = 0; j < items.Count; j++)
-                {
-                    items[j] += "https://proglib.io/vacancies/" + matches2[j].Groups[1].Value + "\n";
-                }
-                
-                for (int j = 0; j < items.Count; j++)
-                {
-                    VacancyListBox.Items.Add(items[j]);
-                }
+                ParallelLoopResult result = Parallel.ForEach<int>(
+        pageNumbers,GetParallelData);
                
+            }
+            catch (Exception ex)
+            {
+                
+               MessageBox.Show(ex.Message);
             }
 
 
